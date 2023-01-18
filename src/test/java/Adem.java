457b66eec1e47bfe5ac4c7b03a4704115d44037e
Adem Basoglu <admbasoglu28@gmail.com>
2023-01-18 12:40:51 +0100

    first commit

---
+++ src/test/java/Adem.java
@@ -0,0 +1,16 @@
+public class Adem {
+    public static void main(String[] args) {
+
+
+        System.out.println("deneme 1");
+
+
+       /* echo "# githubDeneme" >> README.md
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/Admbas/githubDeneme.git
+        git push -u origin main*/
+    }
+}
